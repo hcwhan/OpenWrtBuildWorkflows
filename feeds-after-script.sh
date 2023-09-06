3f68a235e5de8328353e50947c5c36a72b614796
UT003392 <hanchaowei@uniontech.com>
2023-09-06 09:22:29 +0800

    update

--- feeds-after-script.sh
+++ feeds-after-script.sh
@@ -4,8 +4,10 @@
 cd ../
 
 # mosdns start
-rm -rf ./openwrt/feeds/packages/net/v2ray-geodata
-rm -rf ./openwrt/package/feeds/packages/v2ray-geodata
+cd ./openwrt/
+rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./package/feeds/packages/v2ray-geodata
+cd ../
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5  feeds-hcwhan/mosdns
 git clone https://github.com/sbwml/v2ray-geodata          feeds-hcwhan/v2ray-geodata
@@ -15,6 +17,8 @@ sed -i 's/- exec: prefer_ipv4/# - exec: prefer_ipv4/'  feeds-hcwhan/mosdns/luci-
 # mosdns end
 
 [ -e feeds-hcwhan ] && mv feeds-hcwhan/ openwrt/package/feeds/
+
+
 
 cd ./openwrt/
 
