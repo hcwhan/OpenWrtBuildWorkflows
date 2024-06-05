3c1708330a42060a9819bfe322cb6b297379e6df
UT003392 <hanchaowei@uniontech.com>
2024-06-05 13:52:40 +0800

    feat: update

--- feeds-after-script.sh
+++ feeds-after-script.sh
@@ -3,34 +3,38 @@
 
 cd ../
 
-# mosdns start
+# feeds-hcwhan start
 cd ./openwrt/
 rm -rf ./feeds/packages/net/v2ray-geodata
 rm -rf ./package/feeds/packages/v2ray-geodata
 cd ../
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5  feeds-hcwhan/mosdns
-git clone https://github.com/sbwml/v2ray-geodata          feeds-hcwhan/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5  ./feeds-hcwhan/mosdns
+git clone https://github.com/sbwml/v2ray-geodata          ./feeds-hcwhan/v2ray-geodata
 
-sed -i 's/- exec: prefer_ipv4/# - exec: prefer_ipv4/'  feeds-hcwhan/mosdns/luci-app-mosdns/root/etc/mosdns/config_custom.yaml
-sed -i 's/- exec: prefer_ipv4/# - exec: prefer_ipv4/'  feeds-hcwhan/mosdns/luci-app-mosdns/root/usr/share/mosdns/default.yaml
-# mosdns end
+sed -i 's/- exec: prefer_ipv4/# - exec: prefer_ipv4/'     ./feeds-hcwhan/mosdns/luci-app-mosdns/root/etc/mosdns/config_custom.yaml
+sed -i 's/- exec: prefer_ipv4/# - exec: prefer_ipv4/'     ./feeds-hcwhan/mosdns/luci-app-mosdns/root/usr/share/mosdns/default.yaml
 
-[ -e feeds-hcwhan ] && mv feeds-hcwhan/ openwrt/package/feeds/
+mv ./feeds-hcwhan/  ./openwrt/package/feeds/
+# feeds-hcwhan end
 
 
+# change string start
+cd ./openwrt/feeds/luci/
 
-cd ./openwrt/
+sed -i 's/msgstr "CPU 性能优化调节"/msgstr "CPU 频率"/'         ./applications/luci-app-cpufreq/po/zh_Hans/cpufreq.po
+sed -i 's/msgstr "CPU 性能优化调节设置"/msgstr "CPU 频率设置"/'  ./applications/luci-app-cpufreq/po/zh_Hans/cpufreq.po
 
-cd ./feeds/luci
+sed -i 's/msgid "SQM QoS"/msgid "SQM"/'                      ./applications/luci-app-sqm/po/zh_Hans/sqm.po
+sed -i 's/msgstr "SQM 队列管理"/msgstr "队列管理(SQM)"/'        ./applications/luci-app-sqm/po/zh_Hans/sqm.po
 
-sed -i 's/msgstr "CPU 性能优化调节"/msgstr "CPU 频率"/'  applications/luci-app-cpufreq/po/zh_Hans/cpufreq.po
-sed -i 's/msgstr "CPU 性能优化调节设置"/msgstr "CPU 频率设置"/'  applications/luci-app-cpufreq/po/zh_Hans/cpufreq.po
+sed -i 's/"title": "SQM QoS",/"title": "SQM",/'               ./applications/luci-app-sqm/root/usr/share/luci/menu.d/luci-app-sqm.json
+sed -i 's/\t\t"order": 59,//'                                 ./applications/luci-app-sqm/root/usr/share/luci/menu.d/luci-app-sqm.json
 
-sed -i 's/msgid "SQM QoS"/msgid "SQM"/'  applications/luci-app-sqm/po/zh_Hans/sqm.po
-sed -i 's/msgstr "SQM 队列管理"/msgstr "队列管理(SQM)"/'  applications/luci-app-sqm/po/zh_Hans/sqm.po
+cd ../../../
+# change string end
 
-sed -i 's/"title": "SQM QoS",/"title": "SQM",/'  applications/luci-app-sqm/root/usr/share/luci/menu.d/luci-app-sqm.json
-sed -i 's/\t\t"order": 59,//'  applications/luci-app-sqm/root/usr/share/luci/menu.d/luci-app-sqm.json
 
-cd ../../
+# miniupnpd start
+mv ./feeds/miniupnpd/301-change-log.patch  ./openwrt/feeds/packages/net/miniupnpd/patches/
+# miniupnpd end
